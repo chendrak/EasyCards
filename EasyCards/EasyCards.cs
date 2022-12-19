7e1c381dfca6e975c7791a2b408365743130aeff
chendrak <code@haacker.me>
2022-12-18 23:30:55 -0500

    Add game version checks

--- EasyCards/EasyCards.cs
+++ EasyCards/EasyCards.cs
@@ -4,10 +4,12 @@ using EasyCards.CardTypes;
 using EasyCards.Common.Helpers;
 using EasyCards.Effects;
 using EasyCards.Events;
+using EasyCards.Helpers;
 using Il2CppInterop.Runtime;
 using Il2CppInterop.Runtime.Injection;
 using Il2CppInterop.Runtime.InteropTypes.Arrays;
 using ModManager;
+using SemanticVersioning;
 
 namespace EasyCards
 {
@@ -18,8 +20,17 @@ namespace EasyCards
         internal static EasyCards Instance { get; private set; }
         private static ICardLoader CardLoader { get; set; }
 
+        private readonly Version MinimumRequiredGameVersion = new(0, 8, 0);
+
         public override void Load()
         {
+            if (!VersionHelper.IsGameVersionAtLeast(this.MinimumRequiredGameVersion))
+            {
+                Log.LogError($"Wrong game version! Minimum required game version is {this.MinimumRequiredGameVersion}, you have {VersionHelper.GameVersion}");
+                this.Unload();
+                return;
+            }
+
             // This must be set, before resolving anything from the container.
             // As the container uses this to expose BepInEx types.
             Instance = this;
@@ -49,7 +60,8 @@ namespace EasyCards
                 classType.Name
             );
 
-            var ctor = Il2CppType.TypeFromPointer(ptr).GetConstructor((Il2CppReferenceArray<Il2CppSystem.Type>)System.Array.Empty<Il2CppSystem.Type>());
+            var ctor = Il2CppType.TypeFromPointer(ptr)
+                .GetConstructor((Il2CppReferenceArray<Il2CppSystem.Type>)System.Array.Empty<Il2CppSystem.Type>());
             var so = ModGenesia.ModGenesia.AddCustomCard(
                 card.Name,
                 ctor,
